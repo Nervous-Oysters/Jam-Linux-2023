b716b0e4ab040e0145c32e4de3cba042fb4fd78c
NoahFraiture <noah.fraiture@gmail.com>
2023-04-22 17:52:33 +0200

    marchepas

--- src/Actors/Rabbit.gd
+++ src/Actors/Rabbit.gd
@@ -2,26 +2,34 @@ extends CharacterBody2D
 class_name Rabbit
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
+var DIRECTION = 0
+var CURRENT_JUMP = 0.0
+var JUMPING = false
+var LEFT = false
+var RIGHT = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func rabbit_process(delta):
+	# Handle Jump.
+	if is_on_floor() and JUMPING:
+		CURRENT_JUMP = JUMP_VELOCITY
+		velocity.y = CURRENT_JUMP
+	if not is_on_floor() and JUMPING:
+		CURRENT_JUMP -= CURRENT_JUMP * delta*20
+		velocity.y += CURRENT_JUMP
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	if not JUMPING:
+		CURRENT_JUMP = 0.0
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# move left and right
+	if DIRECTION:
+		velocity.x = DIRECTION * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
